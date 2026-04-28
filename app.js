7099b7b200f027298a1908850ef39af0e01db4cd
RahulAnasi <rahulanasibsc@gmail.com>
2026-04-28 09:55:25 +0000

    first commit after db integration

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const http = require('http');
+const mysql = require('mysql2');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from My DevOps Project. We are halfway done and more to come. Be right back!');
+// DB connection
+const connection = mysql.createConnection({
+  host: 'database-2.c5oqk08mi0us.eu-north-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Admin10906',
+  database: 'aws-devops-app-db'
 });
 
-server.listen(3000, '0.0.0.0', () => {
+// Connect to DB
+connection.connect((err) => {
+  if (err) {
+    console.error('DB connection failed:', err);
+    return;
+  }
+  console.log('Connected to DB');
+});
+
+// Create table if not exists
+connection.query(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    text VARCHAR(255)
+  )
+`);
+
+// Create server
+const server = http.createServer((req, res) => {
+
+  // Insert message
+  connection.query(
+    "INSERT INTO messages (text) VALUES ('Our app is now connected to the db. More to come!')"
+  );
+
+  // Fetch messages
+  connection.query(
+    "SELECT * FROM messages",
+    (err, results) => {
+      if (err) {
+        res.end('Error fetching data');
+        return;
+      }
+
+      res.end(JSON.stringify(results));
+    }
+  );
+});
+
+server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
